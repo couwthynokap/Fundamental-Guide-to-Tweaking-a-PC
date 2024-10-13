bfec248a351074a90496fb9d54ad018b2fb54106
e1uen <110753825+e1uen@users.noreply.github.com>
2024-10-13 13:59:26 +0700

    Update cleanup.bat

--- files/cleanup.bat
+++ files/cleanup.bat
@@ -7,9 +7,9 @@ cls
 diskperf -N
 cls
 COMPACT /U /S /A /I /F C:\*.*
-del /s /f /q c:\windows\temp\*.*
-rd /s /q c:\windows\temp
-md c:\windows\temp
+del /s /f /q C:\Windows\temp\*.*
+rd /s /q C:\Windows\temp
+md C:\Windows\temp
 del /s /f /q C:\WINDOWS\Prefetch
 del /s /f /q %temp%\*.*
 rd /s /q %temp%
@@ -20,17 +20,17 @@ del /s /f /q C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Recent
 del /s /f /q C:\Windows\Prefetch
 cls
 cd C:/ & del *.log /a /s /q /f
-del /s /f /q c:windowstemp*.*
-rd /s /q c:windowstemp
-md c:windowstemp
-del /s /f /q C:WINDOWSPrefetch
+del /s /f /q C:Windows\temp*.*
+rd /s /q C:Windows\temp
+md C:Windows\temp
+del /s /f /q C:WINDOWS\Prefetch
 del /s /f /q %temp%*.*
 rd /s /q %temp%
 md %temp%
 for /F " tokens=*" %%G in ('wevtutil.exe el') DO (call :do_clear "%%G")
 ipconfig /flushdns
-rd /s /q "%userprofile%Recycle Bin"
-md "%userprofile%Recycle Bin"
+rd /s /q "%userprofile%\Recycle Bin"
+md "%userprofile%\Recycle Bin"
 compact /c /s C: 
 compact /c /s "%systemdrive%"
 reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\HotStart" /f >NUL 2>&1
@@ -46,9 +46,9 @@ DISM /Online /Remove-Capability /CapabilityName:Browser.InternetExplorer~~0.0.11
 DISM /Online /Remove-Capability /CapabilityName:MathRecognizer0.0.1.0 /norestart /quiet >NUL 2>&1
 DISM /Online /Remove-Capability /CapabilityName:Microsoft.Windows.PowerShell.ISE0.0.1.0 /norestart /quiet >NUL 2>&1
 DISM /Online /Remove-Capability /CapabilityName:OneCoreUAP.OneSync~~0.0.1.0 /norestart /quiet >NUL 2>&1
-del /s /f /q "%windir%temp*.*" >NUL 2>&1
-rd /s /q "%windir%temp" >NUL 2>&1
-md "%windir%temp" >NUL 2>&1
+del /s /f /q "%windir%\temp*.*" >NUL 2>&1
+rd /s /q "%windir%\temp" >NUL 2>&1
+md "%windir%\temp" >NUL 2>&1
 del /s /f /q "%temp%*.*" >NUL 2>&1
 rd /s /q "%temp%" >NUL 2>&1
 md "%temp%" >NUL 2>&1
@@ -61,14 +61,14 @@ for %%A in ("%localappdata%MicrosoftWindowsINetCacheIE*") do (
     del "%%A" /q /f >NUL 2>&1
     rd "%%A" /s /q >NUL 2>&1
 ) >NUL 2>&1
-del /s /f /q "%LOCALAPPDATA%IconCache.db" >NUL 2>&1
-del /s /f /q "%LOCALAPPDATA%MicrosoftWindowsExplorericoncache*" >NUL 2>&1
-del /s /f /q "%LOCALAPPDATA%MicrosoftWindowsExplorerthumbcache*" >NUL 2>&1
+del /s /f /q "%LOCALAPPDATA%\IconCache.db" >NUL 2>&1
+del /s /f /q "%LOCALAPPDATA%\Microsoft\Windows\Explorer\iconcache*" >NUL 2>&1
+del /s /f /q "%LOCALAPPDATA%\Microsoft\Windows\Explorer\thumbcache*" >NUL 2>&1
 powershell Clear-RecycleBin -Force >NUL 2>&1
-del /s /f /q c:windowstemp*.*
-rd /s /q c:windowstemp
-md c:windowstemp
-del /s /f /q C:WINDOWSPrefetch
+del /s /f /q C:Windows\temp*.*
+rd /s /q C:Windows\temp
+md C:Windows\temp
+del /s /f /q C:WINDOWS\Prefetch
 del /s /f /q %temp%*.*
 rd /s /q %temp%
 md %temp%
